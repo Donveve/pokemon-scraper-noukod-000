814a29c94526416c08f666d08924935dfb70ab49
Veniel Dorestave <veniel@noukod.com>
2018-11-12 17:58:22 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,22 +1,22 @@
 class Pokemon
-
-  attr_accessor :id, :name, :type, :hp, :db
-
-def self.save(name, type, db)
-   db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
-end
-
-def self.find(id_num, db)
-  pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).first
-  Pokemon.new(pokemon_info, db)
-end
-
-  def initialize(attr_array, db)
-    @id, @name, @type, @hp = *attr_array
-    @db = db
-  end
-
-  def alter_hp(new_hp)
-    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, id)
-  end
+  @@all = []
+   attr_accessor :id, :name, :type, :db, :hp
+   def initialize(details)
+     details.each {|attribute, value| self.send("#{attribute}=", value)}
+     @@all << self
+   end
+   def alter_hp (hp, db)
+     self.hp = hp
+     db.execute('UPDATE pokemon SET hp = ? WHERE id = ?', hp, id)
+   end
+   def self.all
+     @@all
+   end
+   def self.save(name, type, db)
+     db.execute('INSERT INTO pokemon (name, type) VALUES (?, ?)', name, type)
+   end
+   def self.find(id, db)
+     pokemon = db.execute('SELECT * FROM pokemon WHERE id = ?', id)
+     Pokemon.new(id: pokemon[0][0], name: pokemon[0][1], type: pokemon[0][2], db: db, hp: pokemon[0][3])
+   end
 end
